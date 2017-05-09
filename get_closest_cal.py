49ff17ea7fd70bef52c65711b14f96b465cf167a
kasparsp22 <m6_prusis_k@venta.lv>
2017-05-09 13:03:38 +0000

    Final working version

--- get_closest_cal.py
+++ get_closest_cal.py
@@ -9,10 +9,10 @@ from astropy.coordinates import SkyCoord
 
 
 
-def astropy_sep (tra1,tdec1,tra2,tdes2):
+def astropy_sep (tra1,tdec1,tra2,tdec2):
     sc1 = SkyCoord(tra1,tdec1,frame = 'fk5', unit='degree')
-    sc1 = SkyCoord(tra2,tdec2,frame = 'fk5', unit='degree')
-    return((sc1.speration(sc2)).degree)
+    sc2 = SkyCoord(tra2,tdec2,frame = 'fk5', unit='degree')
+    return((sc1.separation(sc2)).degree)
 
 ######################## MAIN ##############################
 
@@ -28,20 +28,22 @@ content = []
 targets_1 = []
 delta_dist_0 = 1000
 calibrator = None
+
 for i in targets:       ### Removing 'deg' from lists
     targets_1.append([i[0].replace('deg',''),i[1].replace('deg',''),i[2]])
 for i in targets_1:
     for j in calibr:
-        delta_dist_1 =abs(float(i[0])-float(j[0])) + abs(float(i[1]) - float(j[1]))   #### Calculete absoluted distance difference between target and calibratoe
-        if delta_dist_1 < delta_dist_0:   ### Selecting calibrator with the lowest difference 
+        delta_dist_1 = astropy_sep(i[0],i[1],j[0],j[1])  #### Finding seperation between target and cal
+        if delta_dist_1 < delta_dist_0:   ### Selecting closest calibrator
             delta_dist_0 = delta_dist_1
-            calibrator =j[2]
-        outdata =  {
-        "Target": i[2],
-        "Calibrator": calibrator,   
-              }      
-    content.append(outdata)
+            calibrator =j[2] 
+    outdata =  {
+    "Target": i[2],
+    "Calibrator": calibrator,   
+               }
+    delta_dist_0 = 1000  ### To start comparing again
 
+    content.append(outdata)
 ############### Returns output file ################
 
 outfile = open('targets_cals.txt','w')
